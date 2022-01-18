979db46f33d94cec1f2bf18504732320c002dd02
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-01-18 21:12:29 +0300

    practise

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: false,
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-normalize': {},
+    'postcss-nesting': {},
+    'postcss-extend-rule': {},
+    autoprefixer: {}
+  }
+};
