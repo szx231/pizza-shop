63821bc4585b863043bca22eb8499814afc571e2
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-02-02 23:04:10 +0300

    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import '../styles/style.css'
 
 import './includes/test'
+
+const itemSale = document.querySelectorAll('.item-sale__combo');
+const linkSale = document.querySelectorAll('.sale-combo__link');
+
+
