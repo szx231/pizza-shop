aab06ebecefd17970b6fcdff95b020d56bbb5fad
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-03-01 22:22:42 +0300

    Changed some links, css changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import '../styles/style.css'
 import anime from 'animejs/lib/anime.es.js';
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
 import './includes/test'
 
 
@@ -8,28 +10,28 @@ const rootfilter = document.querySelector('.root');
 const modalfilter = document.querySelector('.filter');
 
 
-bodyfilter.forEach(item => {
-  item.addEventListener('click', event => {
-    rootfilter.style.filter = 'blur(10px)';
-    modalfilter.style.display = 'block';
-  })
-})
+// bodyfilter.forEach(item => {
+//   item.addEventListener('click', event => {
+//     rootfilter.style.filter = 'blur(10px)';
+//     modalfilter.style.display = 'block';
+//   })
+// })
 
-const logo = document.querySelectorAll('.delevery__logo');
+// const logo = document.querySelectorAll('.delevery__logo');
 
-logo.forEach(item => {
-  item.addEventListener('click', event => {
-    rootfilter.style.filter = 'blur(10px)';
-    modalfilter.style.display = 'block';
-  })
-})
+// logo.forEach(item => {
+//   item.addEventListener('click', event => {
+//     rootfilter.style.filter = 'blur(10px)';
+//     modalfilter.style.display = 'block';
+//   })
+// })
 
 
-let a = function () {
-  anime({
-  targets: '.dir-alternate',
-  translateX: 250,
-  direction: 'alternate',
-  easing: 'easeInOutSine'
-});
-}
+// let a = function () {
+//   anime({
+//   targets: '.dir-alternate',
+//   translateX: 250,
+//   direction: 'alternate',
+//   easing: 'easeInOutSine'
+// });
+// }
