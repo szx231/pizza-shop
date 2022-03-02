61024d7a32108ccacb8b56c76e9fcf8cc5a4127b
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-03-02 21:40:52 +0300

    add popUp

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,18 @@
 import '../styles/style.css'
 import anime from 'animejs/lib/anime.es.js';
-import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
+import datepicker from 'js-datepicker'
 import './includes/test'
 
 
 const bodyfilter = document.querySelectorAll('body');
 const rootfilter = document.querySelector('.root');
 const modalfilter = document.querySelector('.filter');
+const loginProfile = document.querySelector('.nav-list__linkLOGIN');
 
 
-// bodyfilter.forEach(item => {
-//   item.addEventListener('click', event => {
-//     rootfilter.style.filter = 'blur(10px)';
-//     modalfilter.style.display = 'block';
-//   })
-// })
+loginProfile.addEventListener('click', function() {
+  console.log('1');
+  rootfilter.style.filter = 'blur(10px)';
+  
+})
 
-// const logo = document.querySelectorAll('.delevery__logo');
-
-// logo.forEach(item => {
-//   item.addEventListener('click', event => {
-//     rootfilter.style.filter = 'blur(10px)';
-//     modalfilter.style.display = 'block';
-//   })
-// })
-
-
-// let a = function () {
-//   anime({
-//   targets: '.dir-alternate',
-//   translateX: 250,
-//   direction: 'alternate',
-//   easing: 'easeInOutSine'
-// });
-// }
