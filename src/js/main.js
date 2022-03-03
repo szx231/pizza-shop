31aa7055b5d633d06b1fb89aeb6c35c68fc70e88
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-03-03 22:22:25 +0300

    added popUP some bugs fixed

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,80 @@ import anime from 'animejs/lib/anime.es.js';
 import datepicker from 'js-datepicker'
 import './includes/test'
 
+const body = document.querySelector('body');
+const HTML = document.querySelector('html');
+const lockPaddingValue = window.innerWidth - document.querySelector('.root').offsetWidth + 'px';
+console.log(lockPaddingValue);
 
 const bodyfilter = document.querySelectorAll('body');
 const rootfilter = document.querySelector('.root');
-const modalfilter = document.querySelector('.filter');
 const loginProfile = document.querySelector('.nav-list__linkLOGIN');
+const btnfilter  = document.querySelectorAll('.btnfilter ');
+const filterContainer = document.querySelector('.filter-container');
+const filter = document.querySelector('.filter');
+const filterHeaderButton = document.querySelector('.filter-header__button');
+console.log(filterHeaderButton);
 
+//count 
+const orderCounterPlus = document.querySelectorAll('.order-counter__plus');
+const orderCounterMinus = document.querySelectorAll('.order-counter__minus');
+const orderCounterNumber = document.querySelector('.order-counter__number');
 
-loginProfile.addEventListener('click', function() {
-  console.log('1');
+console.log(orderCounterPlus);
+console.log(orderCounterMinus);
+console.log(orderCounterNumber);
+
+orderCounterPlus.forEach(orderCounterPlus => {
+  orderCounterPlus.addEventListener('click', (event) => {
+    console.log('1');
+  })
+})
+
+function additemCard3Count () {
+  number = 1;
+  if(number > 1) {
+    number++;
+  }
+}
+
+loginProfile.addEventListener('click', () => {
   rootfilter.style.filter = 'blur(10px)';
-  
+})
+
+document.addEventListener('click', function(event) {
+  if(event.target == filterContainer) {
+    filter.classList.remove("filter__open");
+    filterContainer.classList.remove('filter-container__open');
+    rootfilter.style.filter = 'blur(0px)';
+    HTML.style.overflow = 'auto';
+  }
+})
+
+
+document.addEventListener('keydown', function(event) {
+  if(event.keyCode === 27){
+    filter.classList.remove("filter__open");
+    filterContainer.classList.remove('filter-container__open');
+    rootfilter.style.filter = 'blur(0px)';
+    HTML.style.overflow = 'auto';
+  }
+});
+
+btnfilter.forEach(btnfilter => {
+  btnfilter.addEventListener('click', (event) => {
+    filterContainer.classList.add('filter-container__open');
+    filter.classList.add("filter__open");
+    rootfilter.style.filter = 'blur(10px)';
+    HTML.style.overflow = 'hidden';
+    body.style.paddingRight = lockPaddingValue;
+  })
+})
+
+filterHeaderButton.addEventListener('click', (event) => {
+  filter.classList.toggle("filter__open");
+  filterContainer.classList.toggle('filter-container__open');
+  rootfilter.style.filter = 'blur(0px)';
+  HTML.style.overflow = 'auto';
+  event.stopPropagation();
 })
 
