14de4bb9f984cae4e306d05380983521bc919c88
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-03-04 22:19:36 +0300

    fixed link and  work on (server requests) fetch/async/await

--- src/js/main.js
+++ src/js/main.js
@@ -15,7 +15,7 @@ const btnfilter  = document.querySelectorAll('.btnfilter ');
 const filterContainer = document.querySelector('.filter-container');
 const filter = document.querySelector('.filter');
 const filterHeaderButton = document.querySelector('.filter-header__button');
-console.log(filterHeaderButton);
+
 
 //count 
 const orderCounterPlus = document.querySelectorAll('.order-counter__plus');
@@ -80,3 +80,98 @@ filterHeaderButton.addEventListener('click', (event) => {
   event.stopPropagation();
 })
 
+
+
+
+// fetch/axios 
+
+window.addEventListener('DOMContentLoaded', () => {
+  
+  function req() {
+    // const request = new XMLHttpRequest();
+    // request.open("GET", "http://localhost:3000/people");
+    // request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    // request.send();
+    // request.addEventListener("load", function() {
+    //   if (request.status === 200){
+    //     let data = JSON.parse(request.response);
+    //     console.log(data);
+    //     createCards(data);
+    //   }
+    // });
+    getResource('http://localhost:3000/people')
+      .then(data => createCards(data.data))
+      .catch(err => console.error(err));
+    this.remove();
+  }
+  document.querySelector('.button').addEventListener('click', req, {'once': true});
+  
+
+// async function getResource(url) {
+//   const res = await fetch(`${url}`);
+
+//   if(!res.ok) {
+//     throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+//   }
+
+//     return await res.json();
+//   }
+  async function getResource(url) {
+  const res = await axios(`${url}`);
+
+  if(res.status !== 200) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+  }
+
+    return res;
+  }
+
+  function createCards (response) {
+          response.forEach(item => {
+          let card = document.createElement('div');
+          card.classList.add('card');
+          let icon;
+          if(item.sex === 'male') {
+            icon = 'icons/mars.png';
+          } else {
+            icon ='icons/female.png';
+          }
+          card.innerHTML = `
+          <img src="${item.photo}" alt="">
+          <div class="name">${item.name} ${item.surname}</div>
+          <div class="sex">
+            <img src=${icon} alt="male">
+          </div>
+          <div class="age">${item.age}</div>
+          `;
+          document.querySelector('.data-container').appendChild(card);
+        })
+}
+});
+
+
+const dataContainer = document.querySelector('.data-container');
+
+async function getResponse() {
+  let  response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
+  let content = await response.json();
+  content = content.splice(0, 5);
+
+  let list = document.querySelector('.posts');
+  console.log(content);
+  let key;
+  for (key in content) {
+    list.innerHTML += `
+    <li class = 'post'>
+      <h2>${content[key].name}</h2>
+      <h3>${content[key].email}</h3>
+      <h4>${content[key].body}</h4>
+    </li>
+    `
+    console.log(content[key]);
+  }
+}
+
+dataContainer.addEventListener('click', function() {
+  getResponse();
+});
