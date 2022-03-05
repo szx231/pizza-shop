b2774ec58ba972a9c25981223b7550a1b6614fb4
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-03-05 21:49:22 +0300

    work on popUP and fixed some bugs

---
+++ src/js/includes/PopUPLogin.js
@@ -0,0 +1,47 @@
+import { openPopup } from './popups';
+
+const listItemLogin = document.querySelector('.list-item_login');
+const HTML = document.querySelector('html');
+console.log(listItemLogin);
+function renderPopup() {
+  const popup = `
+    <div class="accLoginfilter__wrap">
+      <div class="accLoginfilter">
+        <div class="accLoginfilter__container">
+          <div class="accLoginfilter__title">Вход в аккаунт</div>
+          <button class="accLoginfilter__buttonClose"></button>
+          <div class="accLoginfilter__descr">
+            Сможете быстро оформлять заказы, использовать бонусы
+          </div>
+          <div class="accLoginfilter__number">Номер телефона</div>
+          <input
+            placeholder="+7"
+            type="text"
+            class="accLoginfilter-number__input"
+          />
+          <div class="accLoginfilter__button">Войти</div>
+          <div class="accLoginfilter__agreement">
+            Продолжая, вы соглашаетесь со сбором и обработкой персональных
+            данных и пользовательским соглашением
+          </div>
+        </div>
+      </div>
+    </div>`
+  document.body.insertAdjacentHTML('beforeend', popup);
+  HTML.style.overflow = 'hidden';
+}
+
+
+document.addEventListener('keydown', function(event) {
+  if(event.keyCode === 27){
+    Node.removeChild;
+    rootfilter.style.filter = 'blur(0px)';
+    HTML.style.overflow = 'auto';
+  }
+});
+
+listItemLogin.addEventListener('click', renderPopup);
+
+// renderPopup();
+
+export default renderPopup;
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import '../styles/style.css'
 import anime from 'animejs/lib/anime.es.js';
 import datepicker from 'js-datepicker'
 import './includes/test'
+import renderPopup from './includes/PopUPLogin';
 
 const body = document.querySelector('body');
 const HTML = document.querySelector('html');
@@ -21,10 +22,6 @@ const filterHeaderButton = document.querySelector('.filter-header__button');
 const orderCounterPlus = document.querySelectorAll('.order-counter__plus');
 const orderCounterMinus = document.querySelectorAll('.order-counter__minus');
 const orderCounterNumber = document.querySelector('.order-counter__number');
-
-console.log(orderCounterPlus);
-console.log(orderCounterMinus);
-console.log(orderCounterNumber);
 
 orderCounterPlus.forEach(orderCounterPlus => {
   orderCounterPlus.addEventListener('click', (event) => {
@@ -80,98 +77,4 @@ filterHeaderButton.addEventListener('click', (event) => {
   event.stopPropagation();
 })
 
-
-
-
-// fetch/axios 
-
-window.addEventListener('DOMContentLoaded', () => {
-  
-  function req() {
-    // const request = new XMLHttpRequest();
-    // request.open("GET", "http://localhost:3000/people");
-    // request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    // request.send();
-    // request.addEventListener("load", function() {
-    //   if (request.status === 200){
-    //     let data = JSON.parse(request.response);
-    //     console.log(data);
-    //     createCards(data);
-    //   }
-    // });
-    getResource('http://localhost:3000/people')
-      .then(data => createCards(data.data))
-      .catch(err => console.error(err));
-    this.remove();
-  }
-  document.querySelector('.button').addEventListener('click', req, {'once': true});
-  
-
-// async function getResource(url) {
-//   const res = await fetch(`${url}`);
-
-//   if(!res.ok) {
-//     throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-//   }
-
-//     return await res.json();
-//   }
-  async function getResource(url) {
-  const res = await axios(`${url}`);
-
-  if(res.status !== 200) {
-    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-  }
-
-    return res;
-  }
-
-  function createCards (response) {
-          response.forEach(item => {
-          let card = document.createElement('div');
-          card.classList.add('card');
-          let icon;
-          if(item.sex === 'male') {
-            icon = 'icons/mars.png';
-          } else {
-            icon ='icons/female.png';
-          }
-          card.innerHTML = `
-          <img src="${item.photo}" alt="">
-          <div class="name">${item.name} ${item.surname}</div>
-          <div class="sex">
-            <img src=${icon} alt="male">
-          </div>
-          <div class="age">${item.age}</div>
-          `;
-          document.querySelector('.data-container').appendChild(card);
-        })
-}
-});
-
-
-const dataContainer = document.querySelector('.data-container');
-
-async function getResponse() {
-  let  response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
-  let content = await response.json();
-  content = content.splice(0, 5);
-
-  let list = document.querySelector('.posts');
-  console.log(content);
-  let key;
-  for (key in content) {
-    list.innerHTML += `
-    <li class = 'post'>
-      <h2>${content[key].name}</h2>
-      <h3>${content[key].email}</h3>
-      <h4>${content[key].body}</h4>
-    </li>
-    `
-    console.log(content[key]);
-  }
-}
-
-dataContainer.addEventListener('click', function() {
-  getResponse();
-});
+const btn = document.querySelector('.btn');
